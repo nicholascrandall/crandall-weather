9e185d09deacf86d5f521d38ef068813d860b24d
Nicholas <nickycrandall@gmail.com>
2021-10-19 15:24:29 -0400

    added semantic ui-react

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-        <h2>Centered</h2>
       </div>
     )
   }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
 
 export default class NavBar extends Component {
     constructor(props) {
@@ -10,11 +11,17 @@ export default class NavBar extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Weather</h1>
-                <p>Your Town</p>
-            </div>
+            <>
+            <Menu
+                fixed='top'
+                inverted={true}
+                className='nav-menu'
+            >
+                <Menu.Item name='Home' className='item'>
+                    Home
+                </Menu.Item>
+            </Menu>
+            </>
         )
     }
-
 }
