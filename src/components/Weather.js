202308ab15bc9b76f96fd93a6034c847f1c3ed91
Nicholas <nickycrandall@gmail.com>
2021-10-19 16:02:59 -0400

    added Weather component

---
+++ src/components/Weather.js
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+
+export default class Weather extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apikey: process.env.REACT_APP_API_KEY,
+            city: this.props.city,
+        }
+    }
+
+    render() {
+        // if (this.state.forecast) { }
+        return (
+            <div>
+                <p>Temp: </p>
+                <p>Wind: </p>
+                <p>Description: </p>
+            </div>
+
+        )
+    }
+
+}
