e0120209625771371c9b2b5b873cfe38e8773f9b
Nicholas <nickycrandall@gmail.com>
2021-10-20 17:20:46 -0400

    Updates

---
+++ precache-manifest.7a52cf6228e869e500e8b64f8fd03333.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b41fa94b2b3404eef415034427cb8f",
+    "url": "/crandall-weather/index.html"
+  },
+  {
+    "revision": "d841c67c72b2f8cc9c71",
+    "url": "/crandall-weather/static/css/main.bba5e1e1.chunk.css"
+  },
+  {
+    "revision": "d022b547043ae6bac088",
+    "url": "/crandall-weather/static/js/2.43661294.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/crandall-weather/static/js/2.43661294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56739230f926b6ace5cd",
+    "url": "/crandall-weather/static/js/3.66ebb145.chunk.js"
+  },
+  {
+    "revision": "d841c67c72b2f8cc9c71",
+    "url": "/crandall-weather/static/js/main.48179661.chunk.js"
+  },
+  {
+    "revision": "5f39a89d50b0e09ffd67",
+    "url": "/crandall-weather/static/js/runtime-main.82fa6297.js"
+  },
+  {
+    "revision": "b2590e75752e02903548e160ec56b316",
+    "url": "/crandall-weather/static/media/forecast.b2590e75.png"
+  },
+  {
+    "revision": "00b7d550ffaee95b414ca1464d2103d6",
+    "url": "/crandall-weather/static/media/humidity.00b7d550.png"
+  },
+  {
+    "revision": "3fe95d75fff06369b98e1c5f4942d0e6",
+    "url": "/crandall-weather/static/media/thermometer.3fe95d75.png"
+  },
+  {
+    "revision": "4feb1ea823b15b7948181c0dc68efabb",
+    "url": "/crandall-weather/static/media/wind.4feb1ea8.png"
+  }
+]);
