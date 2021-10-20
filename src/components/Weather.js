8a7ee51b03e150c43b17b5fad217b5960157118b
Nicholas <nickycrandall@gmail.com>
2021-10-19 22:53:42 -0400

    added weathermap fetch call to weather component - BUGGED

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,16 +9,37 @@ export default class Weather extends Component {
         }
     }
 
+    getWeather = () => {
+        const weatherURL = `api.openweathermap.org/data/2.5/weather?zip=${this.state.zipCode}&appid=${this.state.apikey}&units=imperial`
+        fetch(weatherURL)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    forecast: data
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.getWeather()
+    }
+
     render() {
         console.log(`WEATHER PAGE: ${this.state.zipCode}`)
-        // if (this.state.forecast) { }
-        return (
-            <div>
-                <p>Temp: </p>
-                <p>Wind: </p>
-                <p>Description: </p>
-            </div>
-
-        )
+        if (this.state.forecast) {
+            return (
+                <div>
+                    <p>Temp: </p>
+                    <p>Wind: </p>
+                    <p>Description: </p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h3>Weather Details Not Found</h3>
+                </div>
+            )
+        }
     }
 }
